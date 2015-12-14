6b1946a76dea7444285d17c78447076810659329
Robert Brockie <robertbrockie@gmail.com>
2015-12-14 17:11:24 -0500

    Application now implements Flux using Alt.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,60 +1,34 @@
-import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			notes: [
-				{
-					id: uuid.v4(),
-					task: 'Learn Webpack'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		}
-	}
 	render() {
 		const notes = this.state.notes;
 
 		return (
 			<div>
 				<button className="add-note" onClick={this.addNote}>+</button>
-				<Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+				<AltContainer
+					stores={[NoteStore]}
+					inject={{
+						items: () => NoteStore.getState().notes
+					}}
+				>
+					<Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+				</AltContainer>
 			</div>
 		);
 	}
-	addNote = () => {
-		this.setState({
-			notes: this.state.notes.concat([{
-				id: uuid.v4(),
-				task: 'New task'	
-			}])
-		});
+	addNote() {
+		NoteActions.create({task: 'New Task'});
 	}
-	deleteNote = (id) => {
-		this.setState({
-			notes: this.state.notes.filter((note) => note.id !== id)
-		});
+	editNote(id, task) {
+		NoteActions.update({id, task});
 	}
-	editNote = (id, task) => {
-		const notes = this.state.notes.map((note) => {
-			if (note.id === id) {
-				note.task = task;
-			}
-
-			return note;
-		});
-
-		this.setState({notes});
+	deleteNote(id) {
+		NoteActions.delete(id);
 	}
 }
